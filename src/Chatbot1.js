06e1040708dc7a5fff9805735f7a5252d80de4ef
Turmandakh.n <turmandakh.n@itc.gov.mn>
2023-11-02 02:01:58 +0800

    update

--- src/Chatbot1.js
+++ src/Chatbot1.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { question, answer } from './data';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import logo from './img/logo.png'
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import SmartToyOutlinedIcon from '@mui/icons-material/SmartToyOutlined';
@@ -34,44 +34,45 @@ const Chatbot = () => {
   
 
   return (
-    <div className="chat-container">
-
-
-      {messages.map((message, index) => (
-        <div className={message.startsWith(' ') ? 'chatRobot' : 'chatUser'}>
-          {message.startsWith(' ') ? <SmartToyOutlinedIcon fontSize="medium" />: <PersonOutlineOutlinedIcon fontSize="medium" />}
-          <div key={index} className={message.startsWith(' ') ? 'bot-message' : 'user-message'}>
-            {message}
+    <div className='container'>
+      <img src={logo} />
+      <div className="chat-container">
+        {messages.map((message, index) => (
+          <div className={message.startsWith(' ') ? 'chatRobot' : 'chatUser'}>
+            {message.startsWith(' ') ? <SmartToyOutlinedIcon fontSize="medium" />: <PersonOutlineOutlinedIcon fontSize="medium" />}
+            <div key={index} className={message.startsWith(' ') ? 'bot-message' : 'user-message'}>
+              {message}
+            </div>
           </div>
-        </div>
-      ))}
-
-      <div className='questionBox'>
-        {question.map((option, index) => (
-            <button className='button' key={index} onClick={() => 
-              clickme(option.question)
-            }>
-              <div className='question'>
-                {option.question}
-              </div>
-            </button>
         ))}
-      </div>
 
-      <div className='box'>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleSendMessage();
-            }
-          }}
-        />
-        <Button onClick={handleSendMessage} variant="outlined" endIcon={<SendIcon />}></Button>
+        <div className='questionBox'>
+          {question.map((option, index) => (
+              <button className='button' key={index} onClick={() => 
+                clickme(option.question)
+              }>
+                <div className='question'>
+                  {option.question}
+                </div>
+              </button>
+          ))}
+        </div>
+
+        <div className='box'>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSendMessage();
+              }
+            }}
+          />
+          <Button onClick={handleSendMessage} variant="outlined" endIcon={<SendIcon />}></Button>
+        </div>
+        
       </div>
-      
     </div>
   );
 };
